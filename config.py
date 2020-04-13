daa54ef42d074be7908838c7445546bfbe8345c3
David Newman <newmanbdavid@gmail.com>
2020-04-12 20:09:52 -0500

    Added sqlite3 tools

--- config.py
+++ config.py
@@ -1,5 +1,9 @@
 import os
 
 db_info = {
-    "filename": os.path.join(os.getcwd(), "simpletimeentry.db")
+    'filename': os.path.join(os.getcwd(), 'simpletimeentry.db'),
+    'tasks': {'Administration': 'Tasks such as email, preparing for sessions, completing documents, and other duties as assigned',
+              'Trainings': 'Compliance, mandatory, and/or pre-approved or assigned by manager',
+              'Mentoring': 'Assisting a teammate with Microsoft Teams practice/questions',
+              'Coaching sessions': 'Manager/Director led'},
 }
